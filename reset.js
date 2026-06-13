6d039c01a667611df0288acaf554829e099faf12
Marshad <marshadahamedh@gmail.com>
2026-06-13 23:51:40 +0530

    created organized structure for html css and js files

--- reset.js
+++
@@ -1,6 +0,0 @@
-//This js file contains the logic to re set the task list when user clicks filter and want to reset
-const resetButton = document.getElementById('reset-btn')
-
-resetButton.addEventListener('click',()=>{
-    displayTask(array)
-})
