2909f5ca3b55715ab0e87390e6f279528f33c009
Marshad <marshadahamedh@gmail.com>
2026-06-18 11:12:09 +0530

    feat:complted StudyBuddy

--- student-task-manager/js/gpa-calculation-function.js
+++ student-task-manager/js/gpa-calculation-function.js
@@ -30,6 +30,7 @@ function createGpaElements(){
 
         <label for="course-credit-label">Course Credit:</label>
         <select class="course-credit-class">
+            <option value="0">0</option>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
